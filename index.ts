8c620d8fa7a399dae987dd625d53440c37e865ab
hunnble <hante_hunter@163.com>
2019-09-01 21:13:06 +0800

    WIP: get types from AST

--- index.ts
+++ index.ts
@@ -1,12 +1,48 @@
 import * as fs from 'fs'
 import * as ts from 'typescript'
 
-export function loadFile(filename: string) {
-  const fileContent = fs.readFileSync(filename).toString()
-  const sourceFile = ts.createSourceFile(filename, fileContent, ts.ScriptTarget.ES2015, true)
-  return sourceFile
+// export function loadFile(filename: string) {
+//   const fileContent = fs.readFileSync(filename).toString()
+//   const sourceFile = ts.createSourceFile(filename, fileContent, ts.ScriptTarget.ES2019, true)
+//   return sourceFile
+// }
+
+// export function analyzeNode(node: ts.Node) {
+//   console.log(ts.SyntaxKind[node.kind])
+//   console.log('===')
+//   ts.forEachChild(node, analyzeNode)
+// }
+
+// export function analyze(sourceFile: ts.SourceFile) {
+//   analyzeNode(sourceFile)
+// }
+
+// const file = loadFile('./test.ts')
+// analyze(file)
+
+export function analyze(filenames: string[]) {
+  const program = ts.createProgram(filenames, {
+    target: ts.ScriptTarget.ES2019,
+    module: ts.ModuleKind.CommonJS,
+  })
+  const checker = program.getTypeChecker()
+  for (const sourceFile of program.getSourceFiles()) {
+    if (!sourceFile.isDeclarationFile) {
+      ts.forEachChild(sourceFile, analyzeNode)
+    }
+  }
+
+  function analyzeNode(node: ts.Node) {
+    // const symbol = checker.getSymbolAtLocation(node)
+    // if (symbol) {
+    //   console.log(checker.typeToString(
+    //     checker.getTypeOfSymbolAtLocation(symbol, symbol.valueDeclaration!)
+    //   ))
+    // }
+    const type = checker.getTypeAtLocation(node)
+    console.log(checker.typeToString(type))
+    ts.forEachChild(node, analyzeNode)
+  }
 }
 
-export function analyze(sourceFile: ts.SourceFile) {
-  // TODO: analyze
-}
+analyze(['./test.ts'])
