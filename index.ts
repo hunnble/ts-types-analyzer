401428d5bb6eb5af56858cd5e541f3350eaa88e0
hunnble <hante_hunter@163.com>
2019-09-03 22:18:38 +0800

    WIP: get types from AST, remove comments

--- index.ts
+++ index.ts
@@ -1,24 +1,4 @@
 import * as ts from 'typescript'
-// import * as fs from 'fs'
-
-// export function loadFile(filename: string) {
-//   const fileContent = fs.readFileSync(filename).toString()
-//   const sourceFile = ts.createSourceFile(filename, fileContent, ts.ScriptTarget.ES2019, true)
-//   return sourceFile
-// }
-
-// export function analyzeNode(node: ts.Node) {
-//   console.log(ts.SyntaxKind[node.kind])
-//   console.log('===')
-//   ts.forEachChild(node, analyzeNode)
-// }
-
-// export function analyze(sourceFile: ts.SourceFile) {
-//   analyzeNode(sourceFile)
-// }
-
-// const file = loadFile('./test.ts')
-// analyze(file)
 
 export function analyze(filenames: string[]) {
   const program = ts.createProgram(filenames, {
@@ -33,12 +13,6 @@ export function analyze(filenames: string[]) {
   }
 
   function analyzeNode(node: ts.Node) {
-    // const symbol = checker.getSymbolAtLocation(node)
-    // if (symbol) {
-    //   console.log(checker.typeToString(
-    //     checker.getTypeOfSymbolAtLocation(symbol, symbol.valueDeclaration!)
-    //   ))
-    // }
     const type = checker.getTypeAtLocation(node)
     console.log(checker.typeToString(type))
     ts.forEachChild(node, analyzeNode)
